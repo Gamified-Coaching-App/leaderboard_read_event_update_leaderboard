2f8a28dd76230a1630e854fc236d0bdb88c2889a
Varun Khandekar <varunkhandekar@gmail.com>
2024-02-15 16:02:53 +0000

    correcting user_id duplicate

--- index.mjs
+++ index.mjs
@@ -7,24 +7,24 @@ const dynamo_db = new AWS.DynamoDB.DocumentClient();
 export const handler = async (event) => {
     //////////////////////////////////////////////////////////////////////////  
     // Output details to console log -> testing purposes
-    try {
-        // Extract details from the event
-        const { user_id, timestamp_local, session_id, activity_type, distance_in_meters } = event.detail;
+    // try {
+    //     // Extract details from the event
+    //     const { user_id, timestamp_local, session_id, activity_type, distance_in_meters } = event.detail;
 
-        // Log extracted details to the console
-        console.log(`User ID: ${user_id}`);
-        console.log(`Timestamp: ${timestamp_local}`);
-        console.log(`Session ID: ${session_id}`);
-        console.log(`Activity Type: ${activity_type}`);
-        console.log(`Distance in Meters: ${distance_in_meters}`);
-    } catch (error) {
-        console.error(`Error processing event: ${error}`);
-        // Handle the error appropriately
-        return {
-            statusCode: 500,
-            body: JSON.stringify('Error processing event')
-        };
-    }
+    //     // Log extracted details to the console
+    //     console.log(`User ID: ${user_id}`);
+    //     console.log(`Timestamp: ${timestamp_local}`);
+    //     console.log(`Session ID: ${session_id}`);
+    //     console.log(`Activity Type: ${activity_type}`);
+    //     console.log(`Distance in Meters: ${distance_in_meters}`);
+    // } catch (error) {
+    //     console.error(`Error processing event: ${error}`);
+    //     // Handle the error appropriately
+    //     return {
+    //         statusCode: 500,
+    //         body: JSON.stringify('Error processing event')
+    //     };
+    // }
     /////////////////////////////////////////////////////////////////
     const user_id = event.detail.user_id;
 
